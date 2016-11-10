0b7fb9326d46c3ad022741ee6b3560dd387d7daf
Andreas <Andreas@andreass-mbp-5>
2016-11-11 00:07:09 +0100

    test

--- src/aufgabenblatt2/Euler.java
+++ src/aufgabenblatt2/Euler.java
@@ -1,13 +1,36 @@
 package aufgabenblatt2;
 
 public class Euler {
+	
+	private static double calcFactorial(long basis) {
+		double result = 1.0;
+		for(int i = 1; i <= basis; i++) {
+		result = result * i;
+		}
+	return result;
+	}
 
 	public static double myExp(int x) {
 		// TODO berechne e^x mittels der auf Aufgabenblatt 2 angegebenen Formel
+		double result=1;
+		for (int ii=1; ii<= 10; ii++) {
+		result = result + ( Math.pow(x, ii) / calcFactorial(ii) );
+		
+//		System.out.println("Result="+result);
+//		System.out.println("Fakultät="+calcFactorial(ii));
+//		System.out.println();
+		}
+		return result;
 	}
 	
-	public static double myExpFast(int x){
-		// TODO beschleunige myExp()
-	}
+//	public static double myExpFast(int x){
+//		// TODO beschleunige myExp()
+//	}
 
+public static void main(String[] args){
+	
+	double yo=myExp(3);
+	System.out.println("Das Egebniss ist " + yo +".");
+}
+	
 }

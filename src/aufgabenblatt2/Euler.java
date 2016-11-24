42b6dae460d33b8bef8054dfb876ff7670ec354d
nkuehnel <nkuehnel2@gmail.com>
2016-11-24 19:37:53 +0100

    Update Euler.java

--- src/aufgabenblatt2/Euler.java
+++ src/aufgabenblatt2/Euler.java
@@ -1,19 +1,22 @@
 package aufgabenblatt2;
 
+//Gut gelöst. Bitte etwas mehr auf Formatierung achten. 
+//Die Musterlösung für die schnellere Variante werde ich demnächst in das repository laden.
+
 public class Euler {
 	
 	private static double calcFactorial(long basis) {
 		double result = 1.0;
 		for(int i = 1; i <= basis; i++) {
-		result = result * i;
+		result = result * i;		//Formatierung
 		}
-	return result;
+	return result;				//Formatierung
 	}
 
 	public static double myExp(int x, int n) {
 		double result=1;
 		for (int ii=1; ii<= n; ii++) {
-		result = result + ( Math.pow(x, ii) / calcFactorial(ii) );
+		result = result + ( Math.pow(x, ii) / calcFactorial(ii) );	//Formatierung
 		}
 		return result;
 	}
@@ -22,12 +25,12 @@ public class Euler {
 //		// TODO beschleunige myExp()
 //	}
 
-public static void main(String[] args){
+public static void main(String[] args){						//Formatierung
 	
 	int n=17;
 	int x=1;
 	double yo=myExp(x,n);
-	System.out.println("Das Egebniss ist " + yo +".");
-	}
+	System.out.println("Das Egebniss ist " + yo +".");	
+	}									
 	
 }
